e9e04cea9391da1c362cbcc343499e5916265f74
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2023-02-23 23:12:16 +0000

    added burger menu, and a lot of stlying changes to cater for mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,46 @@
-<script setup>
-
-const items = [
-  {
-    imageSrc: '../src/assets/images/carousel/Stock4.jpg',
-    title: 'The Local Maintenance Specialists You Can Trust',
-    subtitle: 'Ensuring your holiday home is in safe hands'
-  },
-  {
-    imageSrc: '../src/assets/images/carousel/Stock2.jpg',
-    title: 'Family Run Business',
-    subtitle: 'Maintaining your family holiday home as if it was our own'
-  },
-  {
-    imageSrc: '../src/assets/images/carousel/Stock3.jpg',
-    title: 'Knowing Your Unknown',
-    subtitle: 'Experienced in knowing what you need before you know you need it!'
-  }
-];
-</script>
-
 <template>
   <Splide :options="{ rewind: true }">
-    <SplideSlide v-for="item in items">
+    <SplideSlide>
       <div class="relative">
-        <img class="max-h-[500px] w-full object-cover" :src="item.imageSrc" alt="">
+        <img class="max-h-[500px] w-full object-cover" src="/src/assets/images/carousel/Stock1.jpg" alt="">
         <div class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50">
           <div class="flex flex-col justify-center h-full">
-            <h1 class="text-5xl font-bold text-center text-white">{{ item.title }}</h1>
-            <h2 class="text-2xl font-bold text-center text-white">{{ item.subtitle }}</h2>
+            <h1 class="text-lg md:text-3xl xl:text-5xl font-bold text-center text-white">The Local Maintenance Specialists You Can Trust</h1>
+            <h2 class="text-base md:text-lg xl:text-2xl font-bold text-center text-white">Ensuring your holiday home is in safe hands</h2>
+          </div>
+        </div>
+      </div>
+    </SplideSlide>
+
+    <SplideSlide>
+      <div class="relative">
+        <img class="max-h-[500px] w-full object-cover" src="/src/assets/images/carousel/Stock2.jpg" alt="">
+        <div class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50">
+          <div class="flex flex-col justify-center h-full">
+            <h1 class="text-lg md:text-3xl xl:text-5xl font-bold text-center text-white">Family Run Business</h1>
+            <h2 class="text-base md:text-lg xl:text-2xl font-bold text-center text-white">Maintaining your family holiday home as if it was our own</h2>
+          </div>
+        </div>
+      </div>
+    </SplideSlide>
+
+    <SplideSlide>
+      <div class="relative">
+        <img class="max-h-[500px] w-full object-cover" src="/src/assets/images/carousel/Stock3.jpg" alt="">
+        <div class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50">
+          <div class="flex flex-col justify-center h-full">
+            <h1 class="text-lg md:text-3xl xl:text-5xl font-bold text-center text-white">Knowing Your Unknown</h1>
+            <h2 class="text-base md:text-lg xl:text-2xl font-bold text-center text-white">Experienced in knowing what you need before you know you need it!</h2>
           </div>
         </div>
       </div>
     </SplideSlide>
   </Splide>
 
-  <div class="py-12 mx-auto flex">
-    <div class="w-1/3 pr-12">
-      <img class="w-full rounded-r-lg" src="/src/assets/images/team/Dave.jpg" alt="Stock Image 1">
-    </div>
-    <div class="h-auto w-1/3 text-white">
+  <div class="p-8 mx-auto flex">
+    <div class="h-auto text-white">
       <h1 class="text-5xl font-bold pb-4 text-center">Welcome</h1>
-      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto, beatae deserunt eaque esse eveniet id maiores nesciunt quae saepe totam unde. Amet architecto asperiores, autem debitis, deleniti eaque esse fuga incidunt necessitatibus nostrum officiis quibusdam rem, repellendus reprehenderit temporibus veniam voluptas voluptate voluptatem voluptates voluptatum? Atque debitis dolor laboriosam necessitatibus suscipit tempora unde voluptate. Ab architecto aspernatur blanditiis consectetur corporis dolor dolores, est exercitationem illo inventore libero nihil, nobis odit optio quam quisquam ratione rem ullam unde ut! Alias assumenda distinctio molestiae? Alias, aliquam commodi, culpa cupiditate deserunt dolores dolorum excepturi inventore odit quae rem sequi similique tempore! Ab, ad aliquid, amet aspernatur aut beatae consectetur cum dolor eveniet excepturi facilis fugiat hic ipsam laboriosam magnam maiores nam necessitatibus, nulla odit omnis optio quo recusandae saepe sapiente sequi voluptatem voluptates. Aliquam dolores eligendi explicabo id in laudantium minima minus nemo, nisi numquam, obcaecati officiis quibusdam recusandae tempore temporibus vel veritatis? Aspernatur debitis ipsa nam placeat quam? Illum ipsam placeat similique. Ex natus placeat quisquam! Aspernatur illo odio odit sint temporibus. Animi at aut consectetur delectus dignissimos doloribus dolorum earum eligendi eos et facilis fuga illo illum ipsam molestias nesciunt nostrum obcaecati pariatur perspiciatis quas, quod, recusandae similique suscipit! Dolores.</p>
-    </div>
-    <div class="w-1/3 pl-12">
-      <img class="w-full rounded-l-lg" src="/src/assets/images/team/Charlotte.jpg" alt="Stock Image 2">
+      <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci architecto atque blanditiis culpa dolores error et facilis ipsum, laudantium libero magni molestiae nam quaerat quam sapiente, sit suscipit veritatis! Asperiores blanditiis culpa deleniti dolor dolorem dolores doloribus dolorum eius enim error in magni minima, minus odit, optio quia reiciendis voluptate.</p>
     </div>
   </div>
 </template>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,53 +1,53 @@
-<script setup>
-  const team = [
-    {
-      Name: 'Dave Pestell',
-      Role: 'Maintenance Lead',
-      Image: 'src/assets/images/team/dave.jpg',
-      Bio: 'Leading the team is founder and owner David, who has over 30-years-experience in the building trade and ' +
-          'expertise in all areas of building work and project management. He’s meticulous in his work, tenacious in ' +
-          'his approach and no challenge is too big or small for Dave. “There’s always ways and means” is one of the ' +
-          'favourites of Dave\'s (many!) catchphrases , and when he’s not out building an extension somewhere you’ll ' +
-          'find him cruising the ‘mean streets’ of Hertfordshire on his Triumph Speedmaster.'
-    },
-    {
-      Name: 'Tracey Pestell',
-      Role: 'Administrator',
-      Image: 'src/assets/images/team/tracey.jpg',
-      Bio: 'Whilst Dave and Brad are busy doing the physical building work, Tracey takes care of the administrative ' +
-          '\'heavy lifting\'. From working with the quantity surveyor to put together job estimates, to responding to ' +
-          'emails and dealing with suppliers, Tracey is always busy working away in the background to help a customer ' +
-          'and support the team. She\'s also an expert at chasing unpaid invoices - you have been warned!'
-    },
-    {
-      Name: 'Charlotte Pestell',
-      Role: 'Client Services',
-      Image: 'src/assets/images/team/charlotte.jpg',
-      Bio: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, alias animi consectetur ' +
-          'consequatur dolor, eum eveniet impedit incidunt nostrum odio ratione suscipit, vitae voluptatem. Ab ' +
-          'aliquid animi aperiam consequatur, dicta, earum ex, exercitationem laborum minus nam placeat quaerat quo ' +
-          'ratione sint soluta vitae voluptatem! Cumque dolores eveniet magnam sequi tempora.'
-    },
-    {
-      Name: 'Brad Pestell',
-      Role: 'Maintenance',
-      Image: 'src/assets/images/team/brad.jpg',
-      Bio: 'Following closely in his father’s footsteps, Brad is a qualified carpenter and joiner with over 10 years ' +
-          'experience in the building trade. He’s a real ‘people person’ and loves nothing more than getting stuck in ' +
-          'and helping customers with their projects. Brad is also a keen motorcyclist and when he’s not out on his ' +
-          'bike you’ll find him out on his mountain bike or playing football.'
-    },
-  ];
-</script>
-
 <template>
   <div class="container mx-auto">
-    <div class="flex">
-      <div v-for="member in team" class="w-1/3 mx-4">
-        <img class="rounded-lg" :src="member.Image" alt="Dave Pestell" />
-        <h2 class="text-white text-4xl my-6">{{ member.Name }}</h2>
-        <h3 class="text-gray-200 text-lg my-1">{{ member.Role }}</h3>
-        <p class="text-gray-300">{{ member.Bio }}</p>
+    <div class="flex flex-wrap">
+      <div class="w-full sm:w-1/2 xl:w-1/4 p-4">
+        <img class="rounded-lg" src="/src/assets/images/team/dave.jpg" alt="Dave Pestell" />
+        <h2 class="text-white text-4xl my-6">Dave Pestell</h2>
+        <h3 class="text-gray-200 text-lg my-1">Maintenance Lead</h3>
+        <p class="text-gray-300">
+          Leading the team is founder and owner David, who has over 30-years-experience in the building trade and
+          expertise in all areas of building work and project management. He’s meticulous in his work, tenacious in
+          his approach and no challenge is too big or small for Dave. “There’s always ways and means” is one of the
+          favourites of Dave's (many!) catchphrases , and when he’s not out building an extension somewhere you’ll
+          find him cruising the ‘mean streets’ of Hertfordshire on his Triumph Speedmaster.
+        </p>
+      </div>
+
+      <div class="w-full sm:w-1/2 xl:w-1/4 p-4">
+        <img class="rounded-lg" src="/src/assets/images/team/tracey.jpg" alt="Tracey Pestell" />
+        <h2 class="text-white text-4xl my-6">Tracey Pestell</h2>
+        <h3 class="text-gray-200 text-lg my-1">Administration</h3>
+        <p class="text-gray-300">
+          Whilst Dave and Brad are busy doing the physical building work, Tracey takes care of the administrative
+          'heavy lifting'. From working with the quantity surveyor to put together job estimates, to responding to
+          emails and dealing with suppliers, Tracey is always busy working away in the background to help a customer
+          and support the team. She's also an expert at chasing unpaid invoices - you have been warned!
+        </p>
+      </div>
+
+      <div class="w-full sm:w-1/2 xl:w-1/4 p-4">
+        <img class="rounded-lg" src="/src/assets/images/team/charlotte.jpg" alt="Charlotte Pestell" />
+        <h2 class="text-white text-4xl my-6">Charlotte Pestell</h2>
+        <h3 class="text-gray-200 text-lg my-1">Client Services</h3>
+        <p class="text-gray-300">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium cupiditate debitis deserunt
+          distinctio eaque harum qui veritatis voluptatibus? Aut, commodi, quos? Aperiam aspernatur, consequuntur
+          ea fugit modi quidem quos rem sequi tempora voluptatibus! Ab architecto assumenda cum cupiditate, esse
+          impedit, maiores modi nesciunt numquam perspiciatis placeat porro possimus, sit totam?
+        </p>
+      </div>
+
+      <div class="w-full sm:w-1/2 xl:w-1/4 p-4">
+        <img class="rounded-lg" src="/src/assets/images/team/brad.jpg" alt="Brad Pestell" />
+        <h2 class="text-white text-4xl my-6">Brad Pestell</h2>
+        <h3 class="text-gray-200 text-lg my-1">Maintenance</h3>
+        <p class="text-gray-300">
+          Following closely in his father’s footsteps, Brad is a qualified carpenter and joiner with over 10 years
+          experience in the building trade. He’s a real ‘people person’ and loves nothing more than getting stuck in
+          and helping customers with their projects. Brad is also a keen motorcyclist and when he’s not out on his
+          bike you’ll find him out on his mountain bike or playing football.
+        </p>
       </div>
     </div>
   </div>
